dbe6195439b58cdd1b56483e434c51418cc6339f
aimonchan <119902688+aimonchan@users.noreply.github.com>
2023-12-05 02:50:56 +0000

    Morning Update from PrimeHill

--- src/Footer.js
+++ src/Footer.js
@@ -3,64 +3,61 @@ function Footer(){
     return(
         <div>
             <footer className="footer-10">
-			
-			<div className="container">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-7">
+							<div className="row">
+								<div className="col-md-4 mb-md-0 mb-4">
+									<h4 className="footer-heading">About</h4>
+									<ul className="list-unstyled">
+									<li><a href="#" className="d-block">Out story</a></li>
+									<li><a href="#" className="d-block">Awards</a></li>
+									<li><a href="#" className="d-block">Our Team</a></li>
+									<li><a href="#" className="d-block">Career</a></li>
+									</ul>
+								</div>
+								
+								<div className="col-md-4 mb-md-0 mb-4">
+									<h4 className="footer-heading">Company</h4>
+									<ul className="list-unstyled">
+									<li><a href="#" className="d-block">Our services</a></li>
+									<li><a href="#" className="d-block">Clients</a></li>
+									<li><a href="#" className="d-block">Contact</a></li>
+									<li><a href="#" className="d-block">Press</a></li>
+									</ul>
+								</div>
 				
-				<div className="row">
-					<div className="col-md-7">
-						<div className="row">
-				<div className="col-md-4 mb-md-0 mb-4">
-					<h4 className="footer-heading">About</h4>
-					<ul className="list-unstyled">
-		              <li><a href="#" className="d-block">Out story</a></li>
-		              <li><a href="#" className="d-block">Awards</a></li>
-		              <li><a href="#" className="d-block">Our Team</a></li>
-		              <li><a href="#" className="d-block">Career</a></li>
-		            </ul>
-				</div>
-				<div className="col-md-4 mb-md-0 mb-4">
-					<h4 className="footer-heading">Company</h4>
-					<ul className="list-unstyled">
-		              <li><a href="#" className="d-block">Our services</a></li>
-		              <li><a href="#" className="d-block">Clients</a></li>
-		              <li><a href="#" className="d-block">Contact</a></li>
-		              <li><a href="#" className="d-block">Press</a></li>
-		            </ul>
-				</div>
-				
-				<div className="col-md-4 mb-md-0 mb-4">
-					<h4 className="footer-heading">Resources</h4>
-					<ul className="list-unstyled">
-		              <li><a href="#" className="d-block">Blog</a></li>
-		              <li><a href="#" className="d-block">Newsletter</a></li>
-		              <li><a href="#" className="d-block">Privacy Policy</a></li>
-		            </ul>
-				</div>
-
-                            
+								<div className="col-md-4 mb-md-0 mb-4">
+									<h4 className="footer-heading">Resources</h4>
+									<ul className="list-unstyled">
+									<li><a href="#" className="d-block">Blog</a></li>
+									<li><a href="#" className="d-block">Newsletter</a></li>
+									<li><a href="#" className="d-block">Privacy Policy</a></li>
+									</ul>
+								</div>
+                        	</div>
 						</div>
 					</div>
-					
-				</div>
+				
 				<div className="row mt-5 pt-4 border-top">
-          <div className="col-md-6 col-lg-8 mb-md-0 mb-4">
-            <p className="copyright mb-0">
+          			<div className="col-md-6 col-lg-8 mb-md-0 mb-4">
+            			<p className="copyright mb-0">
   Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved. | This template is made with <i className="ion-ios-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
   </p>
-          </div>
-          <div className="col-md-6 col-lg-4 d-flex flex-row text-md-right">
-          	<ul className="ftco-footer-social p-0 list-unstyled list-group list-group-horizontal">
-              <li className="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter">
-				<i class="fa-brands fa-square-twitter"></i></a></li>
-              <li className="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook">
-			  <i class="fa-brands fa-square-facebook"></i></a></li>
-              <li className="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram">
-			  <i class="fa-brands fa-square-instagram"></i></a></li>
-            </ul>
-          </div>
-        </div>
+          			</div>
+					<div className="col-md-6 col-lg-4 d-flex flex-row text-md-right">
+						<ul className="ftco-footer-social p-0 list-unstyled list-group list-group-horizontal">
+						<li className="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter">
+							<i class="fa-brands fa-square-twitter"></i></a></li>
+						<li className="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook">
+						<i class="fa-brands fa-square-facebook"></i></a></li>
+						<li className="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram">
+						<i class="fa-brands fa-square-instagram"></i></a></li>
+						</ul>
+					</div>
+        		</div>
 			</div>
-		</footer>
+			</footer>
         </div>
     );
 }
