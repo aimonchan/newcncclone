4da6edae0f9f38878e7ca11f1f7772177131dfca
aimonchan <119902688+aimonchan@users.noreply.github.com>
2023-12-06 10:26:07 +0000

    slightly update from Prime Hill

--- src/Rightbutton.js
+++ src/Rightbutton.js
@@ -4,7 +4,7 @@ function Rightbutton() {
       <div>
             <button className="btn btn-light rounded-pill border-0">Airbnb your home</button>
             <button className="btn btn-light rounded-pill border-0"><i class="fa-solid fa-globe"></i></button>
-            <a type="button" className="btn btn-light rounded-pill d-inline-flex align-items-center justify-content-around border">
+            <a type="button" className="btn btn-light bttn rounded-pill d-inline-flex align-items-center justify-content-around border">
             <div><i className="fa-solid fa-bars me-2"></i></div>
             <div><i className="fa-solid fa-user-circle fa-2x text-secondary me-0 ms-2"></i></div>
             </a>
