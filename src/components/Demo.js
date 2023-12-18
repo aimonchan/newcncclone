4f9e2a9d6436da713b5d4ab87269e3a334d1e535
aimonchan <mr.minaimonchan@gmail.com>
2023-12-18 23:50:06 +0630

    Simple CSS

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -4,32 +4,9 @@ function Demo(){
         <div className="bg-light">
             <div className="center divheight">
                 <div className="btn-group" role="group" aria-label="Basic example">
-                    
-                <div className='dropdown'>
-                    <button className="btn dropdown-toggle" role="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Stays
-                    </button>
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a className="dropdown-item" href="#">Action</a>
-                    <a className="dropdown-item" href="#">Another action</a>
-                    <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </div> 
-
+                    <a className='btn'>Stays</a>              
                     <a className="btn">Experiences</a>
                     <a className="btn">Online Experiences</a>
-
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown button
-                        </button>
-                        <div class="dropdown-menu" role='menu' aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>
-
                 </div>
             </div>
 
@@ -40,18 +17,11 @@ function Demo(){
                         <input type className="form-control form-control-sm no-border focus-ring focus-ring-light" placeholder="Search Destinations"></input>
                     </a>
 
-                    <div className='dropdown w-15'>
-                        <button className="btn bg-light rounded-pill text-start ps-3 pe-3 border dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" type="button" id="dropdownMenuLink">
+                    
+                    <a className="btn bg-light rounded-pill text-start ps-3 pe-3 border">
                             <div><label>Check in</label></div>
                             <div className="text-muted">Add dates</div>
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </div>                    
-                    
+                    </a>
                     <a className="btn bg-light rounded-pill w-15 text-start ps-3 pe-3 border">
                         <div>Check out</div>
                         <div className="text-muted">Add dates</div>
@@ -67,10 +37,14 @@ function Demo(){
                             <span className='ms-1'>Search</span>
                             </button>
                         </div>
-                        
                     </a>
-                    
                 </div>
+            </div>
+
+            <div className="container-fluid bg-none center dropdown-content heightauto d-flex justify-content-center h-50">
+                <div className="worldmap">Hello</div>
+                <div className="worldmapB">worldmapB</div>
+                <div className="worldmapC">worldmapC</div>
             </div>
             
         </div>
